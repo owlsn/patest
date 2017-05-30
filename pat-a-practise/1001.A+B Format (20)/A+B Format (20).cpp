bbc03d7057108815d3d53ddc1e11734d3079308c
owlsn <helloworld.zouhao@gmail.com>
2017-05-30 13:49:44 +0800

    modify answer

--- pat-a-practise/1001.A+B Format (20)/A+B Format (20).cpp
+++ pat-a-practise/1001.A+B Format (20)/A+B Format (20).cpp
@@ -1,6 +1,33 @@
 #include <stdio.h>
 int  main(int argc, char* argv[])
 {
+	int a, b, str[10], i = 0;
+	scanf("%d%d", &a, &b);
+	//计算a+b
+	int sum = a + b;
+	//若sum<0则打印负号
+	if(sum < 0){
+		printf("-");
+		sum = - sum;
+	}
+	//每3位一组
+	int remain = sum;
+	do{
+		str[i] = remain % 1000;
+		i++;
+	}
+	while(remain /= 1000);
+	//第一组开头不需要逗号,其他的前面需要打印逗号，逆序打印
+	for(printf("%d", str[--i]); i; --i){
+		printf(",%03d", str[i-1]);
+	}
+	return 0;
+}
+
+/*
+#include <stdio.h>
+int  main(int argc, char* argv[])
+{
 	int a, b;
 	//输入a,b
 	scanf("%d%d", &a, &b);
@@ -26,3 +53,4 @@ int  main(int argc, char* argv[])
 	}
 	return 0;
 }
+*/
