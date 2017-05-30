71910303f20d3c5e1c700222893d05997a446c65
owlsn <helloworld.zouhao@gmail.com>
2017-05-30 12:16:04 +0800

    pat a 1001 answer

--- pat-a-practise/1001.A+B Format (20)/A+B Format (20).cpp
+++ pat-a-practise/1001.A+B Format (20)/A+B Format (20).cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h
-
-int main(int argc, char* argv[]){
+int  main(int argc, char* argv[])
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	a += b;
+	if(a < 0){
+		printf("-");
+		a = - a;
+	}
+	if(a >= 1000000){
+		printf("%d,%03d,%03d", a/1000000, a%1000000/1000, a%1000);
+	}
+	else if(a > 1000){
+		printf("%d,%03d", a/1000, a%1000);
+	}
+	else{
+		printf("%d", a);
+	}
 	return 0;
 }
