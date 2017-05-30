336f101971cb268d332e0ae0c0d07153c38ee868
owlsn <helloworld.zouhao@gmail.com>
2017-05-30 12:30:29 +0800

    add notes

--- pat-a-practise/1001.A+B Format (20)/A+B Format (20).cpp
+++ pat-a-practise/1001.A+B Format (20)/A+B Format (20).cpp
@@ -2,16 +2,23 @@
 int  main(int argc, char* argv[])
 {
 	int a, b;
+	//输入a,b
 	scanf("%d%d", &a, &b);
+	//a存储和
 	a += b;
+	//a小于0,打印-号
 	if(a < 0){
 		printf("-");
 		a = - a;
 	}
+	//分成3种情况
+	//a>=1000000
+	//a<1000000 && a >= 1000,
+	//a<1000
 	if(a >= 1000000){
 		printf("%d,%03d,%03d", a/1000000, a%1000000/1000, a%1000);
 	}
-	else if(a > 1000){
+	else if(a >= 1000){
 		printf("%d,%03d", a/1000, a%1000);
 	}
 	else{
