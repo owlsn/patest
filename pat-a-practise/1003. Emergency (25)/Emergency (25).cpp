3f621934109900db9a51590ac4cc61b2f4e92da0
owlsn <helloworld.zouhao@gmail.com>
2017-07-29 00:58:35 +0800

    1003

--- pat-a-practise/1003. Emergency (25)/Emergency (25).cpp
+++ pat-a-practise/1003. Emergency (25)/Emergency (25).cpp
@@ -1,59 +1,68 @@
 #include <stdio.h>
-#include <iostream>
 #include <limits.h>
+#include <algorithm>
 using namespace std;
 
-
 int main(int argc, char * argv[]){
-	int n, m, in, des;
+	//n:城市个数,m边条数
+	//dis:点到其他点的最短距离
+	//weight:每个城市的权重
+	//w:到其他城市的权重和
+	//num:最短路径条数
+	//e,路径长度
+	//visit:访问标记
+	//c1,c2:初始城市和目的城市
+	
+	
+	int n, m, c1, c2, dis[500], weight[500],num[500] = {0}, w[500] = {0}, e[500][500];
 	int inf = INT_MAX;
-	int city[500] = {0}, len[500][500] = {0};
-	int dis[500] = {0};
-	for(int i = 0; i< 500; i++){
-		for(int j = 0; j < 500; j++){
-			if(i == j){
-				len[i][j] = 0;
-			}
-			else{
-				len[i][j] = inf;
-			}
-		}
-	}
-	scanf("%d %d %d %d", &n, &m, &in, &des);
+	bool visit[500] = {false};
+	fill(dis, dis + 500, inf);
+	fill(e[0], e[0] + 500*500, inf);
+	scanf("%d %d %d %d", &n, &m, &c1, &c2);
 	for(int i = 0; i < n; i++){
-		scanf("%d", &city[i]);
+		scanf("%d", &weight[i]);
 	}
+	int a, b, c;
 	for(int i = 0; i < m; i++){
-		int s, d;
-		scanf("%d %d", &s, &d);
-		scanf("%d", &len[s][d]);
-	}
-	int start = in;
-	for(int i = 0; i < m; i++){
-		dis[i] = len[start][i];
+		scanf("%d %d %d", &a, &b, &c);
+		e[b][a] = e[a][b] = c;
 	}
 	
-	int visited[500] = {0};
-	visited[start] = 1;
-	int now;
-	for(int i =0; i < n -1; i++){
-		int min = inf;
-		for(int j = 1; j < n; j++){
-			if(visited[j] == 0 && dis[j] < min){
+	//dis[c1] = 0;表示出发点
+	dis[c1] = 0;
+	w[c1] = weight[c1];
+	num[c1] = 1;
+	
+	for(int i = 0; i< n; i++){
+		int u = -1, min = inf;
+		for(int j = 0; j < n; j++){
+			if(visit[j] == false && dis[j] < min){
 				min = dis[j];
-				now = j;
+				u = j;
 			}
 		}
-		visited[now] = 1;
+		if(u == -1){
+			break;
+		}
+		visit[u] = true;
 		for(int v = 0; v < n; v++){
-			if(len[now][v] < inf && len[now][v] + dis[now] < dis[v]){
-				dis[v] = dis[now] + len[now][v];
+			if(visit[v] == false && e[u][v] != inf){
+				if(dis[u] + e[u][v] < dis[v]){
+					dis[v] = dis[u] + e[u][v];
+					w[v] = w[u] + weight[v];
+					num[v] = num[u];
+				}
+				else if(dis[u] + e[u][v] == dis[v]){
+					num[v] += num[u];
+					if(w[u] + weight[v] > w[v]){
+						w[v] = w[u] + weight[v];
+					}
+				}
 			}
 		}
 	}
-	for(int i = 0; i < n; i ++){
-		printf("%d:%d\t", i, dis[i]);
-	}		
-	cin >> n;
+
+	printf("%d %d", num[c2], w[c2]);
 	return 0;
 }
