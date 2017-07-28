e0bdb4f2416bd8c45322976a758d1b6fa0fec34a
owlsn <helloworld.zouhao@gmail.com>
2017-07-28 19:07:22 +0800

    1003

--- pat-a-practise/1003. Emergency (25)/Emergency (25).cpp
+++ pat-a-practise/1003. Emergency (25)/Emergency (25).cpp
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <iostream>
+#include <limits.h>
 using namespace std;
 
 
 int main(int argc, char * argv[]){
 	int n, m, in, des;
-	int city[500] = {0}, len[500][500] = {0}, hashTable[500][500] = {0};
+	int inf = INT_MAX;
+	int city[500] = {0}, len[500][500] = {0};
+	int dis[500] = {0};
+	for(int i = 0; i< 500; i++){
+		for(int j = 0; j < 500; j++){
+			if(i == j){
+				len[i][j] = 0;
+			}
+			else{
+				len[i][j] = inf;
+			}
+		}
+	}
 	scanf("%d %d %d %d", &n, &m, &in, &des);
 	for(int i = 0; i < n; i++){
 		scanf("%d", &city[i]);
@@ -15,31 +28,32 @@ int main(int argc, char * argv[]){
 		scanf("%d %d", &s, &d);
 		scanf("%d", &len[s][d]);
 	}
-	int count = 0, max = 0, l = 0;;
-	int min = 0;
-	if(len[in][des] != 0){
-		count ++;
-		min = city[i];
-	}
 	int start = in;
-	for(int i = 0; i < 500; i++){
-		if((hashTable[start][i] == 0 && hashTable[i][start] == 0) && len[start][i] != 0){
-			max += len[start][i];
-			l += city[i];
-			start = i;
-			hashTable[start][i] = 1;
-			hashTable[i][start] = 1;
-			if(i == des){
-				count ++;
-				continue;
+	for(int i = 0; i < m; i++){
+		dis[i] = len[start][i];
+	}
+	
+	int visited[500] = {0};
+	visited[start] = 1;
+	int now;
+	for(int i =0; i < n -1; i++){
+		int min = inf;
+		for(int j = 1; j < n; j++){
+			if(visited[j] == 0 && dis[j] < min){
+				min = dis[j];
+				now = j;
 			}
 		}
-		if(min < l){
-			min = l;
+		visited[now] = 1;
+		for(int v = 0; v < n; v++){
+			if(len[now][v] < inf && len[now][v] + dis[now] < dis[v]){
+				dis[v] = dis[now] + len[now][v];
+			}
 		}
 	}
-		
-	printf("%d %d", count, min);
+	for(int i = 0; i < n; i ++){
+		printf("%d:%d\t", i, dis[i]);
+	}		
 	cin >> n;
 	return 0;
 }
