2e28e768b86c116c0de296c6432169bc456eb1bd
owlsn <helloworld.zouhao@gmail.com>
2017-07-29 22:14:28 +0800

    1004

--- pat-a-practise/1004. Counting Leaves (30)/Counting Leaves (30).cpp
+++ pat-a-practise/1004. Counting Leaves (30)/Counting Leaves (30).cpp
@@ -1,10 +1,48 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
 using namespace std;
 
 int main(int argc, char * argv[]){
-	queue<int> q;
-	priority_queue<int> p;
+	int node[101] = {0};
+	int non_leaf[101] = {0}, level_num[101] = {0};
+	int num, nonleaf, index, child_num, parent;
+	cin >> num;
+	cin >> nonleaf;
+	if(num == 1){
+		cout << 1;
+	}
+	else{
+		for(int i = 0; i < nonleaf; i++){
+		cin >> parent;
+		non_leaf[parent] = 1;
+		cin >> child_num;
+		for(int j = 0; j < child_num; j++){
+			cin >> index;
+			node[index] = parent; 
+		}
+	}
+	
+	int level, depth = 0;
+	for(int i = 1; i <= num; i++){
+		if(non_leaf[i] == 0 && node[i] != 0){
+			level = 0;
+			parent = i;
+			while(parent != 1){
+				parent = node[parent];
+				level ++;
+			}
+			level_num[level] ++;
+			if(depth < level){
+				depth = level;
+			}
+		}
+	}
+	for(int i = 0; i <= depth; i++){
+		cout << level_num[i];
+		if(i != depth){
+			cout << ' ';
+		}
+	}
+	}
 	return 0;
 }
