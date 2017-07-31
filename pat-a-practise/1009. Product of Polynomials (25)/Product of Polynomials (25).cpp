d13301067106cfcad2ed2b1e73c199c82823210f
owlsn <helloworld.zouhao@gmail.com>
2017-07-31 21:26:26 +0800

    1009

---
+++ pat-a-practise/1009. Product of Polynomials (25)/Product of Polynomials (25).cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char * argv[]){
+	int n, m; 
+	int temp = 0, nmax = 0, mmax = 0;
+	double n1[1001] = {0}, m1[1001] = {0}, t[2002] = {0}, result[2002] = {0};
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> temp;
+		if(i == 0){
+			nmax = temp;
+		}
+		cin >> n1[temp];
+	}
+	cin >> m;
+	for(int i = 0; i < m; i++){
+		cin >> temp;
+		if(i== 0){
+			mmax = temp;
+		}
+		cin >> m1[temp];
+	}
+	for(int i = 0; i <= nmax; i++){
+		for(int j = 0; j <= mmax; j++){
+			t[i + j] = n1[i] * m1[j];
+			result[i + j] += t[i + j];
+		}
+	}
+	int count = 0;
+	for(int i = 0; i <= nmax + mmax ; i ++ ){
+		if(result[i] != 0){
+			count ++ ;
+		}
+	}
+	cout << count << ' ';
+	for(int i = nmax + mmax; i >= 0; i--){
+		if(result[i] != 0){
+			cout << i << ' ';
+			printf("%.1f", result[i]);
+			if(-- count){
+				cout << ' ';
+			}
+		}
+	}
+	return 0;
+}
