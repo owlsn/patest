5f77180a230691026517dc4d03e7e0696ae58d90
owlsn <helloworld.zouhao@gmail.com>
2017-08-02 00:19:23 +0800

    1010

--- pat-a-practise/1010. Radix (25)/Radix (25).cpp
+++ pat-a-practise/1010. Radix (25)/Radix (25).cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char * argv[]){
 	string a = "", b = "", temp = "", res = "";
-	int tag = 0, radix = 0, result = 0, sum = 0;
+	long long int tag = 0, radix = 0, result = 0, sum = 0;
 	cin >> a >> b;
 	cin >> tag >> radix;
 	if(tag == 1){
@@ -19,40 +19,45 @@ int main(int argc, char * argv[]){
 	int l = temp.length();
 	for(int i = 0; i < l; i++){
 		if(temp[i] >= 48 && temp[i] < 57){
-			sum += pow(double(temp[i] - 48), double(l - i));
+			sum += (temp[i] - 48) * pow(double(radix), double(l - i - 1));
 		}
 		else if(temp[i] >= 57){
-			sum += pow(double(temp[i] - 87), double(l - i - 1));
+			sum += (temp[i] - 87) * pow(double(radix), double(l - i -  1));
 		}
 	}
-	int bit = 0, flag = 0;
+	long long int bit = 0, flag = 0;
 	int len = res.length();
-	int i = 0 , t = 0;
+	long long int i = 1 , t = 0;
 	do{
 		t = 0;
-		for(int j = 0; j < len; j ++){
-			if(res[i] >= 48 && res[i] < 57){
-				bit = res[i] - 48;
+		bit = 0;
+		if(len == 1 && res[0] == '0'){
+			if(sum == 0){
+				cout << 1;			
 			}
-			else if(temp[i] >= 57){
-				bit = res[i] - 87;
+			break;
+		}
+		for(int j = 0; j < len; j ++){
+			if(res[j] >= 48 && res[j] < 57){
+				bit = res[j] - 48;
+			}
+			else if(res[j] >= 57){
+				bit = res[j] - 87;
 			}
 			if(bit >= i){
 				break;
 			}
 			else{
-				cout << "bit:" << bit << ";;sum:" << pow(double(bit), double(len - j));
-				t += pow(double(bit), double(len - j));
+				t += bit * pow(double(i), double(len - j - 1));
 			}
 		}
-		cout << "t:"<< t << endl;
 		if(sum == t){
 			cout << i;
 			flag = 1;
 			break;
 		}
 		i ++;
-	}while( i <= 3);
+	}while( sum >= t);
 	if(flag == 0){
 		cout << "Impossible";
 	}
