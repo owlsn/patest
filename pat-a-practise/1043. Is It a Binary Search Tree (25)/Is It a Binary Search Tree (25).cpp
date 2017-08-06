a2480ffff1bba6d88a0bc3732bba968a8891d88e
owlsn <helloworld.zouhao@gmail.com>
2017-08-07 00:55:52 +0800

    1043

---
+++ pat-a-practise/1043. Is It a Binary Search Tree (25)/Is It a Binary Search Tree (25).cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node{
+	int data;
+	Node * left;
+	Node * right;
+	Node(){
+		data = 0;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+Node * insert(Node * &root, int data){
+	if(root == NULL){
+		root = new Node;
+		root->data = data;
+		return root;
+	}
+	if(data < root->data){
+		insert(root->left, data);
+	}
+	else{
+		insert(root->right, data);
+	}
+	return root;
+}
+
+Node * insertImage(Node * &root, int data){
+	if(root == NULL){
+		root = new Node;
+		root->data = data;
+		return root;
+	}
+	if(data >= root->data){
+		insert(root->left, data);
+	}
+	else{
+		insert(root->right, data);
+	}
+	return root;
+}
+
+void pre(Node * root, vector<int> &seq){
+	if(root == NULL) return;
+	seq.push_back(root->data);
+	pre(root->left, seq);
+	pre(root->right, seq);
+	
+}
+
+void post(Node * root, vector<int> &seq){
+	if(root == NULL)return ;
+	post(root->left, seq);
+	post(root->right, seq);
+	seq.push_back(root->data);
+}
+
+int main(int argc, char * argv[]){
+	int num, index;
+	vector<int> seq, pre_seq, pre_image, post_seq;
+	Node * root = NULL, * image_root = NULL;
+	cin >> num;
+	for(int i = 0; i < num; i ++){
+		cin >> index;
+		seq.push_back(index);
+		root = insert(root, index);
+		image_root = insertImage(image_root, index);
+	}
+	pre(root, pre_seq);
+	pre(image_root, pre_image);
+	cout << image_root->right->right->data << endl;
+	bool flag = false;
+	for(int i = 0; i < num; i++){
+			cout << pre_image[i];
+			if(i != num - 1){
+				cout << " ";
+			}
+		}
+	if(seq == pre_seq){
+		flag = true;
+		post(root, post_seq);
+	}
+	else if(seq == pre_image){
+		flag = true;
+		post(image_root, post_seq);
+	}
+	if(flag){
+		cout << "YES" << endl;
+		for(int i = 0; i < num; i++){
+			cout << post_seq[i];
+			if(i != num - 1){
+				cout << " ";
+			}
+		}
+	}
+	else{
+		cout << "NO";
+	}
+	return 0;
+}
