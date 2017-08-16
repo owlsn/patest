33a7bd79cf08f0c04bc2d86beefacf26f2ab34a9
owlsn <helloworld.zouhao@gmail.com>
2017-08-16 11:21:14 +0800

    1013

--- pat-a-practise/1013. Battle Over Cities (25)/Battle Over Cities (25).cpp
+++ pat-a-practise/1013. Battle Over Cities (25)/Battle Over Cities (25).cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int road[1000][1000] = {0};
+
+void dfs(int root, int visited[], const int n){
+	for(int i = 1; i <= n ; i++){
+		//cout << "des:" << i << endl;
+		if(visited[i] == 0 && (road[root][i] == 1 || road[i][root] == 1)){
+			visited[i] = 1;
+			dfs(i, visited, n);
+		}
+	}
+}
+
 int main(int argc, char * argv[]){
 	int n, m, k, a, b;
-	int road[1000][1000] = {0};
-	int concern[1000] = {0}, visited[1001] = {0};
+	int concern[1000] = {0};
 	cin >> n >> m >> k;
 	for(int i = 0; i < m; i ++){
 		cin >> a >> b;
@@ -16,26 +27,27 @@ int main(int argc, char * argv[]){
 	for(int i = 0; i < k; i ++){
 		int visited[1001] = {0};
 
-		int count = 0;
-		int start = 1;
+		int count = 0, start = 0;
 		visited[concern[i]] = 1;
+		//cout << "concern[i]:" << concern[i] << endl;
 		for(int c = 1; c <= n; c ++){
-			if(visited[c] == 0 && c != start){
-				count ++;
+			if(visited[c] == 0){
 				start = c;
 			}
 			else{
 				continue;
 			}
-			visited[c] == 1;
-			for(int a = 1; a <= n; a ++){
-				if(visited[a] == 0 && (road[start][a] == 1 || road[a][start] == 1)){
-					visited[a] = 1;
-					start = a;
-				}
-			}
+			visited[start] == 1;
+			//cout << "start:" << start << endl;
+			dfs(start, visited, n);
+			count ++ ;
 		}
-		cout << count - 1;
+		if(count > 0){
+			cout << count - 1;
+		}
+		else{
+			cout << 0;
+		}
 		if(i != k - 1){
 			cout << endl;
 		}
