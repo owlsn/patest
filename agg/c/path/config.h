3d2cc544a28bd391aeade841a3834fd1f9bea479
owlsn <helloworld.zouhao@gmail.com>
2018-11-12 15:42:52 +0800

    cmake

---
+++ agg/c/path/config.h
@@ -0,0 +1,6 @@
+#define USE_LIB
+#define PATH_VERSION_MAJOR 1
+#define PATH_VERSION_MINOR 01
+#include "lib/check.h"
+#include "lib/path.h"
+#include "extra/math.h"
--- agg/c/path/main.c
+++ agg/c/path/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-#include"path.h"
-#include"check.h"
+#include "config.h"
+#ifdef USE_LIB
+#include "extra/math.h"
+#endif
 
-int main()
+#ifdef HAVE_POW
+#include <math.h>
+#endif
+
+int main(int argc, char *argv[]) 
 {
     //初始化图的状态
-    int graph[MAXNODE][MAXNODE];
+    int graph[MAXNODE][MAXNODE]; 
     int node[MAXNODE] = {0};
     for (int i = 0; i < MAXNODE; i++)
     {
@@ -26,10 +32,47 @@ int main()
             }
         }
     }
+/**
+ * main
+ * */
+#ifdef USE_LIB
+    // printf("USE USE_LIB\n");
+#else
+    // printf("USE CMATH LIB\n");
+#endif
+
+#ifdef HAVE_POW
+    double a = 2.0, b = 8.0;
+    double res = (double)pow(a, b); 
+    printf("use default pow function , res is :%lg\n", res);
+#else
+    double a = 2.0;
+    int b = 8;
+    double res = pw(a, b);
+    printf("use lib pow function , res is %lg\n", res);
+#endif
+
+if(argc == 3){
+    double base = atof(argv[1]);
+    int exponent = atoi(argv[2]); 
+    double r = pw(base, exponent);
+    printf("%g ^ %d is %g\n", base, exponent, r);
+}
+else{
+    printf("%s Version %d.%d\n",
+               argv[0],
+               PATH_VERSION_MAJOR,
+               PATH_VERSION_MINOR); 
+    printf("Usage: %s base exponent \n", argv[0]);
+}
+return 0;
+/**
+ * 输出
+ * */
     int result[MAXNODE] = {0};
     int pathsNum = 0;
     struct Pathslist *list = init_paths();
-    DFS(graph, MAXNODE, 15, 0, &pathsNum, list);
+    // DFS(graph, MAXNODE, 15, 0, &pathsNum, list);
     if(list->num){
         struct Paths *temp;
         struct PathsNode *n;
