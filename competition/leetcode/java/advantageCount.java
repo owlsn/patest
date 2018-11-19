4ebb5dd98428ce1ee1e70dfa0078d603728aacef
owlsn <helloworld.zouhao@gmail.com>
2018-11-19 23:16:16 +0800

    add leetcode

---
+++ competition/leetcode/java/advantageCount.java
@@ -0,0 +1,11 @@
+public class advantageCount {
+
+    public static void main(String args[]){
+        System.out.println("123");
+    }
+
+    public int[] advantageCount(int[] A, int[] B) {
+        int[] result = new int[10];
+        return result;
+    }
+}
